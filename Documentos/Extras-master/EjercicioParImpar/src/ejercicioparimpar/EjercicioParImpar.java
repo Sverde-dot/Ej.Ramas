13529bf1998f0a85e6e40c54373764ec7689c39f
sverdecadilla <sverdecadilla@danielcastelao.org>
2019-11-27 13:45:03 +0100

    Cambios clase

---
+++ Documentos/Extras-master/EjercicioParImpar/src/ejercicioparimpar/EjercicioParImpar.java
@@ -0,0 +1,14 @@
+package ejercicioparimpar;
+
+/**
+ *
+ * @author irodriguezsteuerberg
+ */
+public class EjercicioParImpar {
+
+    public static void main(String[] args) {
+Metodos obx = new Metodos();
+obx.verDatos();
+    }
+    
+}
