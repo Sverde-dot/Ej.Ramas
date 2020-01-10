0a2bb7d746938e47a012691d29458ebb679e797a
Sverde-dot <55793525+Sverde-dot@users.noreply.github.com>
2020-01-10 13:51:28 +0100

    Ahora fin de la cita

--- src/ejemploholamundo/EjemploHolaMundo.java
+++ src/ejemploholamundo/EjemploHolaMundo.java
@@ -17,6 +17,7 @@ public class EjemploHolaMundo {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("HOLA MUNDO");
+        System.out.println("Fin de la cita");
     }
     
 }
